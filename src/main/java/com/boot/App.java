6e5f65fa96a3464220a6a963c583d8e2246277f3
mhaddon <dis_connected@ymail.com>
2016-05-27 18:54:45 +0100

    first publish

---
+++ src/main/java/com/boot/App.java
@@ -0,0 +1,14 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Admin on 27/05/2016.
+ */
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+}
