f43e1d407fcb5a77dc6a2608e90f031592f45016
mhaddon <dis_connected@ymail.com>
2016-06-08 18:25:02 +0100

    updating maven

--- src/main/java/com/boot/repository/CharacterRepository.java
+++ src/main/java/com/boot/repository/CharacterRepository.java
@@ -5,5 +5,4 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
-    List<Character> findByName(String Name);
 }
